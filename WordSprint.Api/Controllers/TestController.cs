869c51e446dc397ce6beae3b02b94204c606416b
emretutun <memretutun@gmail.com>
2026-02-17 14:05:17 +0300

    chore: add test controller

---
+++ WordSprint.Api/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WordSprint.Api.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
